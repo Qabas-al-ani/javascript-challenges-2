02e02553f9a181771252c3398dee8a0006fafddf
Qabas <qabasalani1@gmail.com>
2022-04-14 13:14:15 -0400

    challange 1 longest word solution

---
+++ index.js
@@ -0,0 +1,19 @@
+// CHALLENGES ONE
+// COUNTING LETTERS JAVASCRIPT
+
+function longestWord(str) {
+  let words = str.split(" ");
+  let longestWord = "";
+
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+}
+const output = longestWord(
+  "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
+);
+console.log(output);
