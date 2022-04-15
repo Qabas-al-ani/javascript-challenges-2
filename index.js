9594ffd1bd5962b7b49b7c04922eaf90686a6eb6
Qabas <qabasalani1@gmail.com>
2022-04-15 17:22:52 -0400

    challange longestword  solution

--- index.js
+++ index.js
@@ -46,4 +46,25 @@
 // challenge 3 Longest word
 // SOLUTION
 
-function longestWord(str) {}
+function longestWord(str) {
+  let words = str.split(" ");
+  let size = 0;
+  let max = [""];
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= size) {
+      size = words[i].length;
+
+      if (max[max.length - 1].length < words[i].length) {
+        max = [];
+        max.push(words[i]);
+      } else {
+        max = [...max, words[i]];
+      }
+    }
+  }
+
+  return [...max];
+}
+
+console.log(longestWord("we source these positions directly from hundreds"));
