d7077b3d8dce5eef924266236cf46fdecfdbbe41
Qabas <qabasalani1@gmail.com>
2022-04-15 17:07:01 -0400

    challange longestword

--- index.js
+++ index.js
@@ -21,23 +21,29 @@
 // CHALLENGE 2  MATH SEQUENCE
 // SOLUTION
 
-function mathSequence(arr) {
-  let arith = new Set();
-  let geo = new Set();
+// function mathSequence(arr) {
+//   let arith = new Set();
+//   let geo = new Set();
 
-  for (let i = 1; i < arr.length; i++) {
-    let number1 = arr[i] - arr[i - 1];
-    arith.add(number1);
-    let number2 = arr[i] / arr[i - 1];
-    geo.add(number2);
-  }
+//   for (let i = 1; i < arr.length; i++) {
+//     let number1 = arr[i] - arr[i - 1];
+//     arith.add(number1);
+//     let number2 = arr[i] / arr[i - 1];
+//     geo.add(number2);
+//   }
 
-  if (arith.size === 1) {
-    return "Arithmetic";
-  }
-  if (geo.size === 1) {
-    return "Geometry";
-  }
-  return -1;
-}
-console.log(mathSequence([2, 4, 6, 8]));
+//   if (arith.size === 1) {
+//     return "Arithmetic";
+//   }
+//   if (geo.size === 1) {
+//     return "Geometry";
+//   }
+//   return -1;
+// }
+// console.log(mathSequence([2, 4, 6, 8]));
+
+////////////////
+// challenge 3 Longest word
+// SOLUTION
+
+function longestWord(str) {}
