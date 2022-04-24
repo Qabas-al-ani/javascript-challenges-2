6bfc7388607cf83b4342a74c5ca38fcb0773270a
Qabas <qabasalani1@gmail.com>
2022-04-24 14:03:44 -0400

    paractiving dizz buzz algorithms 4

--- index.js
+++ index.js
@@ -218,5 +218,17 @@
 // console.log(capitalizeLetter("qaBas al aNI"));
 /////////////////////
 function fizzBuzz() {
+  for (let i = 1; i < 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
   return fizzBuzz;
 }
+console.log(fizzBuzz(100));
