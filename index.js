d12446da3df78b6cd265cac6b71b6568f59525ef
Qabas <qabasalani1@gmail.com>
2022-04-25 15:44:01 -0400

    paractiving algorithms 9 solution add all

--- index.js
+++ index.js
@@ -301,17 +301,22 @@
 // console.log(isAnagram("elbow", "below"));
 ////////////////////////
 // solution
-function letterChanges(str) {
-  let newStr = str.toLowerCase().replace(/[a-z]/gi, function (char) {
-    if (char === "z" || char === "Z") {
-      return "a";
-    } else {
-      return String.fromCharCode(char.charCodeAt() + 1);
-    }
-  });
-  newStr = newStr.replace(/[a|e|i|o|u]/gi, function (vowel) {
-    return vowel.toUpperCase();
-  });
-  return newStr;
+// function letterChanges(str) {
+//   let newStr = str.toLowerCase().replace(/[a-z]/gi, function (char) {
+//     if (char === "z" || char === "Z") {
+//       return "a";
+//     } else {
+//       return String.fromCharCode(char.charCodeAt() + 1);
+//     }
+//   });
+//   newStr = newStr.replace(/[a|e|i|o|u]/gi, function (vowel) {
+//     return vowel.toUpperCase();
+//   });
+//   return newStr;
+// }
+// console.log(letterChanges("Hello There"));
+////////////////////////////////
+function addAll(...numbers) {
+  return numbers.reduce((acc, cur) => acc + cur);
 }
-console.log(letterChanges("Hello There"));
+console.log(addAll(1, 2, 3));
