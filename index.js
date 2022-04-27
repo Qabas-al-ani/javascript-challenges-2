9ba023eaea67156d174e26c0a299e6c2a1ff6fc6
Qabas <qabasalani1@gmail.com>
2022-04-27 19:50:25 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -423,5 +423,16 @@
 // console.log(fizzBuzz(100));
 //////////////////////////////////
 function longestWord(sen) {
-  return sen;
+  //  filter the sen match
+  let senArray = sen.toLowerCase().match(/[a-z0-9]+/g);
+  // sort by length
+  const sorted = senArray.sort(function (a, b) {
+    return b.length - a.length;
+  });
+  // if multiple words
+  const longestWordArray = sorted.filter(function (word) {
+    return word.length === sorted[0].length;
+  });
+  return longestWordArray;
 }
+console.log(longestWord("Qabas, al ani"));
