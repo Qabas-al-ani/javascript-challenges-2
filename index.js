8674c84d9701a5f6954837b22991116098ece9be
Qabas <qabasalani1@gmail.com>
2022-04-27 11:25:20 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -408,5 +408,16 @@
 
 ///////////////////////////////////
 function fizzBuzz() {
-  return fizzBuzz;
+  for (let i = 1; i < 100; i++) {
+    if (i % 15 === 0) {
+      console.log("Fizz Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
+console.log(fizzBuzz(100));
