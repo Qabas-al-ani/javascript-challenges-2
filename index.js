92f21e9c3e97a10b80bfda67879b1d19b4aff504
Qabas <qabasalani1@gmail.com>
2022-04-29 11:09:10 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -534,8 +534,23 @@
 // }
 // console.log(isPalindrome("madams"));
 ////////////////////
-function reversedInt(int) {
-  let newInt = int.toString().split("").reverse().join("");
-  return parseInt(newInt);
+// function reversedInt(int) {
+//   let newInt = int.toString().split("").reverse().join("");
+//   return parseInt(newInt);
+// }
+// console.log(reversedInt(712));
+//////////////////////
+function fizzBuzz() {
+  for (let i = 1; i < 100; i++) {
+    if (i % 15 === 0) {
+      console.log("Fizz Buzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
-console.log(reversedInt(712));
+console.log(fizzBuzz(100));
