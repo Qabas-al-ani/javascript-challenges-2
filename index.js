8625c433d852a20cd12f52f62ff223d2770ffc20
Qabas <qabasalani1@gmail.com>
2022-05-09 21:49:07 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -828,8 +828,12 @@
 // }
 // console.log(isPalindrome("madam"));
 ////////////////////////////////
-function isPalindrome(str) {
-  const strArr = str.split("").reverse().join("");
-  return strArr === str;
-}
-console.log(isPalindrome("madam"));
+// function isPalindrome(str) {
+//   const strArr = str.split("").reverse().join("");
+//   return strArr === str;
+// }
+// console.log(isPalindrome("madam"));
+//////////////////
+
+function reverseString(str) {}
+console.log(reverseString("qabas"));
