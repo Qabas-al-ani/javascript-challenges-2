d4218891452ec1e9e449b4a09c83366204a93886
Qabas <qabasalani1@gmail.com>
2022-05-09 22:30:03 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -859,5 +859,23 @@
 // }
 // console.log(capitalizeLetter("qaBAs aL ani"));
 ////////////////////
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = "";
+  str.split("").forEach(function (char) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 console.log(maxCharacter("qabas"));
