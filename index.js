f717995cc5a993c219ac9e4cc04f254e13ab25a8
Qabas <qabasalani1@gmail.com>
2022-05-13 20:01:23 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -923,19 +923,21 @@
 // }
 // console.log(isAnagram("below", "elbow"));
 //////////////////////////
-function letterChanges(str) {
-  // return formatString(str1) === formatString(str2);
-  // function formatString(str) {
-  //   return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
-  // }
-  //////////////////////////
-}
-console.log(letterChanges("hello there"));
-///////////////////
-function letterChanges(str) {
-  return formatString(str1) === formatString(str2);
-  function formatString(str) {
-    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
-  }
-}
-console.log(letterChanges("hello there"));
+// function letterChanges(str) {
+//   // return formatString(str1) === formatString(str2);
+//   // function formatString(str) {
+//   //   return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+//   // }
+//   //////////////////////////
+// }
+// console.log(letterChanges("hello there"));
+// ///////////////////
+// function letterChanges(str) {
+//   return formatString(str1) === formatString(str2);
+//   function formatString(str) {
+//     return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+//   }
+// }
+// console.log(letterChanges("hello there"));
+//////////////////////////////////
+function letterChanges(str) {}
