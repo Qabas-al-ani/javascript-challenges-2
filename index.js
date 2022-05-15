658daaa716bb6148bcd656c1b573286f3e4f6dc4
Qabas <qabasalani1@gmail.com>
2022-05-14 21:07:42 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -954,19 +954,22 @@
 //   }
 // }
 /////////////////////////
-function longestWord(str) {
-  let words = str.split(" ");
-  let longestWord = "";
+// function longestWord(str) {
+//   let words = str.split(" ");
+//   let longestWord = "";
 
-  for (let word of words) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  }
+//   for (let word of words) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
 
-  return longestWord;
-}
-const output = longestWord(
-  "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
-);
-console.log(output);
+//   return longestWord;
+// }
+// const output = longestWord(
+//   "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
+// );
+// console.log(output);
+///////////////////////////
+function maxCharacter(str) {}
+console.log(maxCharacter("qabas"));
