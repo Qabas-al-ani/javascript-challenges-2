f75e0445e5810441cb8d8225faa14c19e05c56a1
Qabas <qabasalani1@gmail.com>
2022-05-15 21:11:54 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -971,23 +971,26 @@
 // );
 // console.log(output);
 ///////////////////////////
-function maxCharacter(str) {
-  const charMap = {};
-  let maxNum = 0;
-  let maxChar = "";
-  str.split("").forEach(function (char) {
-    if (charMap[char]) {
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
-    }
-  });
-  for (let char in charMap) {
-    if (charMap[char] > maxNum) {
-      maxNum = charMap[char];
-      maxChar = char;
-    }
-  }
-  return maxChar;
-}
+// function maxCharacter(str) {
+//   const charMap = {};
+//   let maxNum = 0;
+//   let maxChar = "";
+//   str.split("").forEach(function (char) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   });
+//   for (let char in charMap) {
+//     if (charMap[char] > maxNum) {
+//       maxNum = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar;
+// }
+// console.log(maxCharacter("qabas"));
+///////////////////////////////////
+function maxCharacter(str) {}
 console.log(maxCharacter("qabas"));
