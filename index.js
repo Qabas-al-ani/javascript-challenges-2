f0b87a1f266c012874d17da41756bcca4c86792c
Qabas <qabasalani1@gmail.com>
2022-05-17 21:25:30 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1069,14 +1069,17 @@
 // }
 // console.log(longestWordArray("Hello, my name is Qabas"));
 //////////////////////
-function longestWordArray(str) {
-  let newWord = str.toLowerCase().match(/[a-z0-9]+/g);
-  let sortedArray = newWord.sort(function (a, b) {
-    return b.length - a.length;
-  });
-  let longestWord = sortedArray.filter(function (word) {
-    word.length === longestWord[0].length;
-  });
-  return longestWord;
-}
-console.log(longestWordArray("Hello, my name is Qabas"));
+// function longestWordArray(str) {
+//   let newWord = str.toLowerCase().match(/[a-z0-9]+/g);
+//   let sortedArray = newWord.sort(function (a, b) {
+//     return b.length - a.length;
+//   });
+//   let longestWord = sortedArray.filter(function (word) {
+//     word.length === longestWord[0].length;
+//   });
+//   return longestWord;
+// }
+// console.log(longestWordArray("Hello, my name is Qabas"));
+//////////////////////////////
+function letterChanges(str) {}
+console.log(letterChanges("hello there"));
