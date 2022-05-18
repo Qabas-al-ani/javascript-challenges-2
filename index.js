4f036c741314a3fe235636252e1c66db955f9fbb
Qabas <qabasalani1@gmail.com>
2022-05-17 21:33:06 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1154,5 +1154,24 @@
 // }
 // console.log(isAnagram("hello", "olleh"));
 /////////////////////////////
-function repeatedLetter(str) {}
+function repeatedLetter(str) {
+  let charMap = {};
+  let maxNum = 0;
+  let maxChar = "";
+  str.split("").forEach(char => {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+    charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+  });
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 console.log(repeatedLetter("javascriptssssssssss"));
