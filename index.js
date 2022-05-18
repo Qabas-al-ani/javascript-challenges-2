ac2ee8402d1be46b75d9949f641d5e42bdf5ab3d
Qabas <qabasalani1@gmail.com>
2022-05-17 21:28:01 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1089,19 +1089,22 @@
 // }
 // console.log(letterChanges("hello there"));
 //////////////////////////////
-function longestWord(str) {
-  let words = str.split(" ");
-  let longestWord = "";
+// function longestWord(str) {
+//   let words = str.split(" ");
+//   let longestWord = "";
 
-  for (let word of words) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  }
+//   for (let word of words) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
 
-  return longestWord;
-}
-const output = longestWord(
-  "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
-);
-console.log(output);
+//   return longestWord;
+// }
+// const output = longestWord(
+//   "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
+// );
+// console.log(output);
+//////////////////////////////////////
+function birthdayCakeCandles(candles) {}
+console.log(birthdayCakeCandles(24));
