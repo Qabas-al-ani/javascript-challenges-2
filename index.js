3c8eaa0fd921bb4548873ba0775e26458e5fc778
Qabas <qabasalani1@gmail.com>
2022-05-19 20:47:45 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1282,5 +1282,8 @@
 // }
 // console.log(capitalizeLetter("qaBAs aL ani"));
 /////////////////////
-function reversedInt(int) {}
+function reversedInt(int) {
+  let newInt = int.toString().split("").reverse().join("");
+  return parseInt(newInt) * Math.sign(int);
+}
 console.log(reversedInt(-12345));
