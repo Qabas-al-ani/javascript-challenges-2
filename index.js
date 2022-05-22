7bdd0945fed4b85a6dba49f5f9eeb27ea5e2a956
Qabas <qabasalani1@gmail.com>
2022-05-22 11:03:10 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1329,7 +1329,25 @@
 // CHALLENGE 2  MATH SEQUENCE
 // SOLUTION
 
-function mathSequence(arr) {}
+function mathSequence(arr) {
+  let arith = new Set();
+  let geo = new Set();
+
+  for (let i = 1; i < arr.length; i++) {
+    let number1 = arr[i] - arr[i - 1];
+    arith.add(number1);
+    let number2 = arr[i] / arr[i - 1];
+    geo.add(number2);
+  }
+
+  if (arith.size === 1) {
+    return "Arithmetic";
+  }
+  if (geo.size === 1) {
+    return "Geometry";
+  }
+  return -1;
+}
 console.log(mathSequence([2, 4, 6, 8]));
 
 ////////////////
