6732e89c35bf103552a104fdd29cfea94f51204d
Qabas <qabasalani1@gmail.com>
2022-05-22 11:02:51 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1309,46 +1309,28 @@
 // CHALLENGES ONE
 // COUNTING LETTERS JAVASCRIPT
 
-function longestWord(str) {
-  let words = str.split(" ");
-  let longestWord = "";
+// function longestWord(str) {
+//   //   let words = str.split(" ");
+// //   let longestWord = "";
 
-  for (let word of words) {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  }
+// //   for (let word of words) {
+// //     if (word.length > longestWord.length) {
+// //       longestWord = word;
+// //     }
+// //   }
 
-  return longestWord;
-}
-const output = longestWord(
-  "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
-);
-console.log(output);
+// //   return longestWord;
+// }
+// const output = longestWord(
+//   "Welcome to the 2U job board. Updated daily, we source these positions directly from hundreds of employer partners across a variety of industries. We work closely with partners to understand their hiring needs so we can best advocate for you, our 2U-powered program learners. If you have any questions, please contact"
+// );
+// console.log(output);
 
 // CHALLENGE 2  MATH SEQUENCE
 // SOLUTION
 
-// function mathSequence(arr) {
-//   let arith = new Set();
-//   let geo = new Set();
-
-//   for (let i = 1; i < arr.length; i++) {
-//     let number1 = arr[i] - arr[i - 1];
-//     arith.add(number1);
-//     let number2 = arr[i] / arr[i - 1];
-//     geo.add(number2);
-//   }
-
-//   if (arith.size === 1) {
-//     return "Arithmetic";
-//   }
-//   if (geo.size === 1) {
-//     return "Geometry";
-//   }
-//   return -1;
-// }
-// console.log(mathSequence([2, 4, 6, 8]));
+function mathSequence(arr) {}
+console.log(mathSequence([2, 4, 6, 8]));
 
 ////////////////
 // challenge 3 Longest word
