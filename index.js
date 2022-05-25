82ea16358c233a4ca4d4c5b488c1f79f1e9df0d3
Qabas <qabasalani1@gmail.com>
2022-05-25 14:21:35 -0400

    paractiving algorithms solution

--- index.js
+++ index.js
@@ -1536,5 +1536,24 @@
 // }
 // console.log(capitalizeLetter("hello my name is qabas "));
 /////////////////////
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  let charMap = {};
+  let maxNum = 0;
+  let maxChar = "";
+
+  str.split("").forEach(function (char) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 console.log(maxCharacter("qabas"));
